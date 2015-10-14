f54b5f6ac63dcda00f11c4321eadaa79f07ff6f7
Bjørn Johansen <post@bjornjohansen.no>
2015-10-14 14:52:55 +0200

    Update year in copyright comment

--- openam-rest.php
+++ openam-rest.php
@@ -22,7 +22,7 @@ Text Domain: openam-auth
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2014 ForgeRock AS.
+ * Copyright 2014-2015 ForgeRock AS.
  */
 
 defined( 'ABSPATH' ) or die();
